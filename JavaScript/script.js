911a0823b86c9a49859bbd8b808a6741bdf64dd6
onzyx <140368083+onzyx@users.noreply.github.com>
2023-09-12 15:30:21 +0700

    Update script.js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,3 +1,10 @@
+// JavaScript to set the video width to match the title width
+window.addEventListener('load', () => {
+    const title = document.getElementById('pageTitle');
+    const video = document.getElementById('myVideo');
+    video.style.width = getComputedStyle(title).width;
+});
+
 // JavaScript to handle the mute/unmute functionality
 const video = document.getElementById('myVideo');
 const soundButton = document.getElementById('soundButton');
