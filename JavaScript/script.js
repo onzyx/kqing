c681e6cc785200f97236d7fb5c89d66281e2c5a2
onzyx <140368083+onzyx@users.noreply.github.com>
2023-09-13 06:40:40 +0700

    Update script.js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,3 +1,21 @@
+// JavaScript to create and move bubble text
+const bubbleContainer = document.getElementById('bubble-container');
+
+function createBubble() {
+    const bubble = document.createElement('div');
+    bubble.className = 'bubble';
+    bubble.textContent = 'Teks Gelembung';
+    bubble.style.top = `${Math.random() * 80 + 10}%`; // Posisi vertikal acak
+    bubbleContainer.appendChild(bubble);
+
+    // Hapus gelembung setelah animasi selesai
+    bubble.addEventListener('animationiteration', () => {
+        bubble.remove();
+    });
+}
+
+setInterval(createBubble, 3000); // Buat gelembung baru setiap 3 detik
+
 // JavaScript to handle the mute/unmute functionality
 const video = document.getElementById('myVideo');
 const soundButton = document.getElementById('soundButton');
