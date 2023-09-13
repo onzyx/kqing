ba61fcb0152c18ccf85beb88c2f85b09475549dc
onzyx <140368083+onzyx@users.noreply.github.com>
2023-09-13 11:44:50 +0700

    Update script.js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -21,6 +21,43 @@ function toggleSound() {
     }
 }
 
+// JavaScript to handle the settings button click
+const settingsButton = document.getElementById('settingsButton');
+settingsButton.addEventListener('click', openSettings);
+
+function openSettings() {
+    // Templat HTML untuk dialog pengaturan
+    const settingsDialog = `
+        <div id="settingsOverlay" class="settings-overlay">
+            <div class="settings-dialog">
+                <h2>Pengaturan</h2>
+                <label for="headerBackgroundColor">Warna Latar Belakang Header:</label>
+                <input type="color" id="headerBackgroundColor">
+                <button id="closeSettingsButton">Tutup</button>
+            </div>
+        </div>
+    `;
+
+    // Menambahkan templat dialog pengaturan ke dalam body
+    document.body.insertAdjacentHTML('beforeend', settingsDialog);
+
+    // Mendapatkan elemen-elemen dalam dialog pengaturan
+    const settingsOverlay = document.getElementById('settingsOverlay');
+    const closeSettingsButton = document.getElementById('closeSettingsButton');
+    const headerBackgroundColorInput = document.getElementById('headerBackgroundColor');
+    const header = document.querySelector('header');
+
+    // Mengatur warna latar belakang header berdasarkan input pengguna
+    headerBackgroundColorInput.addEventListener('input', () => {
+        header.style.backgroundColor = headerBackgroundColorInput.value;
+    });
+
+    // Menutup dialog pengaturan saat tombol Tutup diklik
+    closeSettingsButton.addEventListener('click', () => {
+        settingsOverlay.remove();
+    });
+}
+
 // JavaScript to disable horizontal scrolling
 window.addEventListener('DOMContentLoaded', () => {
     const scrollableElements = document.querySelectorAll('.scrollable');
