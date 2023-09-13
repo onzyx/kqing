458b8b82732b3a16c21b10885ce23084c5784231
onzyx <140368083+onzyx@users.noreply.github.com>
2023-09-13 10:59:16 +0700

    Update script.js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,20 +1,9 @@
-// JavaScript to create and move bubble text
-const bubbleContainer = document.getElementById('bubble-container');
-
-function createBubble() {
-    const bubble = document.createElement('div');
-    bubble.className = 'bubble';
-    bubble.textContent = 'Teks Gelembung';
-    bubble.style.top = `${Math.random() * 80 + 10}%`; // Posisi vertikal acak
-    bubbleContainer.appendChild(bubble);
-
-    // Hapus gelembung setelah animasi selesai
-    bubble.addEventListener('animationiteration', () => {
-        bubble.remove();
-    });
-}
-
-setInterval(createBubble, 3000); // Buat gelembung baru setiap 3 detik
+// JavaScript to set the video width to match the title width
+window.addEventListener('load', () => {
+    const title = document.getElementById('pageTitle');
+    const video = document.getElementById('myVideo');
+    video.style.width = getComputedStyle(title).width;
+});
 
 // JavaScript to handle the mute/unmute functionality
 const video = document.getElementById('myVideo');
@@ -25,9 +14,32 @@ soundButton.addEventListener('click', toggleSound);
 function toggleSound() {
     if (video.muted) {
         video.muted = false;
-        soundButton.textContent = '🔊';
+        soundButton.textContent = '🔊 Matikan Suara';
     } else {
         video.muted = true;
-        soundButton.textContent = '🔇';
+        soundButton.textContent = '🔇 Nyalakan Suara';
     }
 }
+
+// JavaScript to create and move bubble text
+const bubbleContainer = document.getElementById('bubble-container');
+
+function createBubble() {
+    const bubble = document.createElement('div');
+    bubble.className = 'bubble';
+    bubble.textContent = 'Teks Gelembung';
+    
+    const randomPosition = Math.random() * (window.innerHeight - 100); // Posisi vertikal acak di dalam viewport
+    const randomDelay = Math.random() * 10 + 2; // Waktu muncul acak (2-12 detik)
+    
+    bubble.style.top = `${randomPosition}px`;
+    bubble.style.animationDelay = `${randomDelay}s`; // Set waktu muncul acak
+    bubbleContainer.appendChild(bubble);
+
+    // Hapus gelembung setelah animasi selesai
+    bubble.addEventListener('animationiteration', () => {
+        bubble.remove();
+    });
+}
+
+setInterval(createBubble, 5000); // Buat gelembung baru setiap 5 detik
