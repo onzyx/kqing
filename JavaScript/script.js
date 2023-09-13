79285e4d9a3f2d36c1e257a74e599ba18feebf41
onzyx <140368083+onzyx@users.noreply.github.com>
2023-09-13 11:35:49 +0700

    Update script.js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -14,35 +14,12 @@ soundButton.addEventListener('click', toggleSound);
 function toggleSound() {
     if (video.muted) {
         video.muted = false;
-        soundButton.textContent = '🔊';
+        soundButton.textContent = '🔊 Matikan Suara';
     } else {
         video.muted = true;
-        soundButton.textContent = '🔇';
+        soundButton.textContent = '🔇 Nyalakan Suara';
     }
 }
-
-// JavaScript to create and move bubble text
-const bubbleContainer = document.getElementById('bubble-container');
-
-function createBubble() {
-    const bubble = document.createElement('div');
-    bubble.className = 'bubble';
-    bubble.textContent = 'Teks Gelembung';
-
-    const randomPosition = Math.random() * (window.innerHeight - 100); // Posisi vertikal acak di dalam viewport
-    const randomDelay = Math.random() * 10 + 2; // Waktu muncul acak (2-12 detik)
-
-    bubble.style.top = `${randomPosition}px`;
-    bubble.style.animationDelay = `${randomDelay}s, ${-randomDelay}s`; // Set waktu muncul dan animasi secara bersamaan
-    bubbleContainer.appendChild(bubble);
-
-    // Hapus gelembung setelah animasi selesai
-    bubble.addEventListener('animationiteration', () => {
-        bubble.remove();
-    });
-}
-
-setInterval(createBubble, 5000); // Buat gelembung baru setiap 5 detik
 
 // JavaScript to disable horizontal scrolling
 window.addEventListener('DOMContentLoaded', () => {
