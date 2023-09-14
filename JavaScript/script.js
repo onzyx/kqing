e02b87acc1fba90ee471846b5bfc34b2784a34e5
onzyx <140368083+onzyx@users.noreply.github.com>
2023-09-14 11:02:53 +0700

    Update script.js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,17 +1,33 @@
-// JavaScript to handle the mute/unmute functionality
-const video = document.getElementById('myVideo');
-const soundButton = document.getElementById('soundButton');
-let isMuted = true;
+// Generate a random number between 1 and 100
+const randomNumber = Math.floor(Math.random() * 100) + 1;
 
-soundButton.addEventListener('click', toggleSound);
+// Initialize variables
+const guessInput = document.getElementById('guessInput');
+const guessSubmit = document.getElementById('guessSubmit');
+const message = document.getElementById('message');
+let attempts = 0;
 
-function toggleSound() {
-    if (isMuted) {
-        video.muted = false;
-        soundButton.textContent = '🔊';
+// Function to handle a guess
+function checkGuess() {
+    const userGuess = parseInt(guessInput.value);
+    attempts++;
+
+    if (userGuess === randomNumber) {
+        message.textContent = `Selamat! Anda berhasil menebak angka ${randomNumber} dalam ${attempts} percobaan.`;
+        message.style.color = 'green';
+        guessInput.disabled = true;
+        guessSubmit.disabled = true;
+    } else if (userGuess < randomNumber) {
+        message.textContent = 'Tebakan terlalu rendah. Coba lagi!';
+        message.style.color = 'red';
     } else {
-        video.muted = true;
-        soundButton.textContent = '🔇';
+        message.textContent = 'Tebakan terlalu tinggi. Coba lagi!';
+        message.style.color = 'red';
     }
-    isMuted = !isMuted;
+
+    guessInput.value = '';
+    guessInput.focus();
 }
+
+// Event listener for the guess submit button
+guessSubmit.addEventListener('click', checkGuess);
