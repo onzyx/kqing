f40308c0189fb0403423876d67bb01b00354ef8a
onzyx <140368083+onzyx@users.noreply.github.com>
2023-09-22 13:17:25 +0700

    Update script.js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,17 +1,23 @@
-// JavaScript to handle the mute/unmute functionality
-const video = document.getElementById('myVideo');
-const soundButton = document.getElementById('soundButton');
-let isMuted = true;
+const statusElement = document.getElementById('status');
+const toggleButton = document.getElementById('toggleButton');
+const serialPort = new SerialPort();
 
-soundButton.addEventListener('click', toggleSound);
+toggleButton.addEventListener('click', () => {
+  const currentState = toggleButton.innerText.toLowerCase();
+  if (currentState === 'nyalakan') {
+    serialPort.write('1'); // Mengirim perintah '1' ke Arduino.
+  } else if (currentState === 'matikan') {
+    serialPort.write('0'); // Mengirim perintah '0' ke Arduino.
+  }
+});
 
-function toggleSound() {
-    if (isMuted) {
-        video.muted = false;
-        soundButton.textContent = '🔊';
-    } else {
-        video.muted = true;
-        soundButton.textContent = '🔇';
-    }
-    isMuted = !isMuted;
-}
+serialPort.addEventListener('data', (event) => {
+  const data = event.data.trim();
+  if (data === '1') {
+    statusElement.innerText = 'Lampu saat ini: Nyala';
+    toggleButton.innerText = 'Matikan';
+  } else if (data === '0') {
+    statusElement.innerText = 'Lampu saat ini: Mati';
+    toggleButton.innerText = 'Nyalakan';
+  }
+});
